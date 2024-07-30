9f08f7ca741e648adbd8ba9a830e453710dd072b
Jennifer Blanc <jgblanc@uchicago.edu>
2024-07-30 16:14:42 -0500

    t

--- code/Simulate_Genotypes/compute_b2.R
+++ code/Simulate_Genotypes/compute_b2.R
@@ -18,10 +18,17 @@ df <- inner_join(dfE, dfPop)
 # Manually create population PCs
 df <- df %>% mutate(popPC1 = case_when(POP == "A" ~ 1, POP == "B" ~ 0, POP == "C" ~ 0),
                     popPC2 = case_when(POP == "A" ~ 0, POP == "B" ~ 1, POP == "C" ~ -1))
+df$popPC1 <- df$popPC1 - mean(df$popPC1)
+df$popPC2 <- df$popPC2 - mean(df$popPC2)
+
+# Make vectors length 1
+df$popPC1_length1 <- df$popPC1 / sqrt(sum(df$popPC1^2))
+df$popPC2_length1 <- df$popPC2 / sqrt(sum(df$popPC2^2))
+
 
 # Calculate b2
-b2_PC1 <- cor(df$PC1, df$popPC1)^2
-b2_PC2 <- cor(df$PC2, df$popPC2)^2
+b2_PC1 <-(sum(df$PC1 * df$popPC1_length1))^ 2
+b2_PC2 <- (sum(df$PC2 * df$popPC2_length1))^ 2
 
 # Set up output table
 dfOut <- as.data.frame(matrix(NA, nrow = 1, ncol = 2))
